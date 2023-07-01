b165994630f3b84fd2682eee1488a0f65be2fcc1
Reuben John <reubenvjohn@gmail.com>
2023-07-01 11:37:57 -0700

    Setup requests-html library

--- main.py
+++ main.py
@@ -1,16 +1,14 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from requests_html import HTMLSession
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print("Hi, {0}".format(name))  # Press Ctrl+F8 to toggle the breakpoint.
+def main():
+    session = HTMLSession()
+    r = session.get('https://pythonclock.org')
+    print(r.html.search('Python 2.7 will retire in...{}Enable Guido Mode')[0])
+    r.html.render()
+    print(r.html.search('Python 2.7 will retire in...{}Enable Guido Mode')[0])
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    main()
